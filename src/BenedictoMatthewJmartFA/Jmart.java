45be1c5630e49b2a7229fd67926f0f7097efbfd0
Serpenoxxx <matthewwatulingas17@gmail.com>
2021-11-04 15:40:41 +0700

    PostTest mod 6

--- src/BenedictoMatthewJmartFA/Jmart.java
+++ src/BenedictoMatthewJmartFA/Jmart.java
@@ -1,9 +1,35 @@
 package BenedictoMatthewJmartFA;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
+
 public class Jmart
 {
+    class Country
+    {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String[] args) {
-        //Account m = new Account(1, "Meti", "inisimamat@gmail.com", "P4ssw0rd");
-        //System.out.println(m.Validate());
+        String filepath = "D:\\College\\Sem3\\OOP\\jmart\\src\\BenedictoMatthewJmartFA\\city.json";
+        Gson gson = new Gson();
+        try
+        {
+           BufferedReader br = new BufferedReader(new FileReader(filepath));
+           Country input = gson.fromJson(br, Country.class);
+           System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: " );
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }  
 }
